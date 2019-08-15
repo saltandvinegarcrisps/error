8515a0e795219253b5b596c4df279ca0b4bbb09e
Graham Newton <gpn273@users.noreply.github.com>
2019-08-15 15:07:28 +0100

    Update PSR Handler to correctly set severity

--- src/Handler/PsrHandler.php
+++ src/Handler/PsrHandler.php
@@ -2,6 +2,7 @@
 
 namespace Error\Handler;
 
+use Monolog\Logger;
 use Psr\Log\LoggerInterface;
 use Throwable;
 
@@ -18,6 +19,39 @@ class PsrHandler implements HandlerInterface
 
     public function handle(Throwable $e): void
     {
+        if ($e instanceof \ErrorException) {
+            $this->handleErrorException($e);
+            return;
+        }
+
         $this->logger->error($this->getMessage($e), ['exception' => $e]);
+    }
+
+    protected function handleErrorException(\ErrorException $e) {
+        switch ($e->getSeverity()) {
+            case E_ERROR:
+            case E_RECOVERABLE_ERROR:
+            case E_CORE_ERROR:
+            case E_COMPILE_ERROR:
+            case E_USER_ERROR:
+            case E_PARSE:
+                $this->logger->error($this->getMessage($e), ['exception' => $e]);
+                break;
+            case E_WARNING:
+            case E_USER_WARNING:
+            case E_CORE_WARNING:
+            case E_COMPILE_WARNING:
+                $this->logger->warning($this->getMessage($e), ['exception' => $e]);
+                break;
+            case E_NOTICE:
+            case E_USER_NOTICE:
+                $this->logger->notice($this->getMessage($e), ['exception' => $e]);
+                break;
+            case E_STRICT:
+            case E_DEPRECATED:
+            case E_USER_DEPRECATED:
+                $this->logger->info($this->getMessage($e), ['exception' => $e]);
+                break;
+        }
     }
 }
